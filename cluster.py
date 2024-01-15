5dc2780ed8fa8080cd9bd34a7ba9efcde6b217f3
wancat <wancat@wancat.cc>
2024-01-15 23:05:51 +0800

    use cosine similarity, refactor cluster (extract functions)

--- cluster.py
+++ cluster.py
@@ -1,5 +1,6 @@
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.metrics import silhouette_samples
+from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 
 from bookmark_manager import BookmarkManager
@@ -21,42 +22,49 @@ class ClusterManager:
         self.manager = manager
         self.embedder = embedder
         self.model = AgglomerativeClustering(n_clusters=None, distance_threshold=2)
-        # self.clusterModel = ClusterModel()
-        self.embeddings_2d = None
-        self.embeddings_3d = None
         self.n_clusters = 0
         self.cluster_labels = None
-
-    def fit(self, embeddings):
-        self.model.fit(embeddings)
-        return self.model.n_clusters_, self.model.labels_
 
     def cluster(self):
         self.n_clusters, self.cluster_labels = self.fit(self.manager.embeddings)
         self.manager.set_clusters(self.cluster_labels)
 
-        clustered_bookmarks = {}
-        silhouette_vals = silhouette_samples(
-            self.manager.embeddings, self.cluster_labels
-        )
-        silhouette_result = {}
-        for sentence_id, cluster_id in enumerate(self.cluster_labels):
-            if cluster_id not in clustered_bookmarks:
-                clustered_bookmarks[cluster_id] = []
-                silhouette_result[cluster_id] = 0
-
-            clustered_bookmarks[cluster_id].append(self.manager.bookmarks[sentence_id])
-            silhouette_result[cluster_id] += silhouette_vals[sentence_id]
+        clustered_bookmarks = self.cluster_bookmarks()
+        silhouette_scores = self.silhouette_scores()
 
         clusters = []
         for i, bookmarks in clustered_bookmarks.items():
-            silhouette_score = silhouette_result[i] / len(bookmarks)
-            cluster = ClusterInfo(i, bookmarks, score=silhouette_score)
+            score = silhouette_scores[i]
+            cluster = ClusterInfo(i, bookmarks, score=score)
             cluster.title = self.select_title(cluster)
             clusters.append(cluster)
 
         clusters.sort(key=lambda x: x.score, reverse=True)
         self.manager.set_cluster_info(clusters)
+
+    def fit(self, embeddings):
+        self.model.fit(embeddings)
+        return self.model.n_clusters_, self.model.labels_
+
+    def cluster_bookmarks(self):
+        clustered_bookmarks = {i: [] for i in range(self.n_clusters)}
+        for sentence_id, cluster_id in enumerate(self.cluster_labels):
+            clustered_bookmarks[cluster_id].append(self.manager.bookmarks[sentence_id])
+        return clustered_bookmarks
+
+    def silhouette_scores(self):
+        silhouette_vals = silhouette_samples(
+            self.manager.embeddings, self.cluster_labels
+        )
+        silhouette_result = {i: 0 for i in range(self.n_clusters)}
+        silhouette_counts = {i: 0 for i in range(self.n_clusters)}
+        for sentence_id, cluster_id in enumerate(self.cluster_labels):
+            silhouette_result[cluster_id] += silhouette_vals[sentence_id]
+            silhouette_counts[cluster_id] += 1
+
+        for cluster_id in silhouette_result:
+            silhouette_result[cluster_id] /= silhouette_counts[cluster_id]
+        return silhouette_result
 
     def show(self):
         for cluster in self.manager.clusters:
@@ -73,6 +81,7 @@ class ClusterManager:
 
         topic_vectors = self.embedder.embed(topics)
 
-        distances = np.sqrt(((topic_vectors - mean) ** 2).sum(axis=1))
-        closest_topic = np.argmin(distances)
+        similarities = cosine_similarity([mean], topic_vectors)[0]
+
+        closest_topic = np.argmax(similarities)
         return topics[closest_topic]
