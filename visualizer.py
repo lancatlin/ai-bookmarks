ceef1403480834e81391d797da9009c6ff71c27e
wancat <wancat@wancat.cc>
2024-01-15 23:05:13 +0800

    display title on hover

--- visualizer.py
+++ visualizer.py
@@ -1,6 +1,7 @@
 from sklearn.decomposition import PCA
 import numpy as np
 import matplotlib.pyplot as plt
+import mplcursors
 
 from bookmark_manager import BookmarkManager
 
@@ -13,26 +14,42 @@ class Visualizer:
         pca = PCA(n_components=2)
         self.embeddings_2d = pca.fit_transform(self.manager.embeddings)
         print(self.embeddings_2d.shape)
+        cluster_labels = [
+            cluster.title
+            for cluster in sorted(self.manager.clusters, key=lambda x: x.id)
+        ]
+        print(cluster_labels)
         colors = plt.cm.rainbow(np.linspace(0, 1, self.manager.n_clusters()))
         print(colors.shape)
         plt.figure(figsize=(10, 8))
-        for bookmark, (x, y) in zip(self.manager.bookmarks, self.embeddings_2d):
-            color = colors[bookmark.cluster]
-            plt.scatter(x, y, color=color)
-            # plt.text(
-            #     x + 0.01, y + 0.01, self.manager.bookmarks[i].title, fontsize=9
-            # )  # Adjust text position and size
+        x = self.embeddings_2d[:, 0]
+        y = self.embeddings_2d[:, 1]
+        color = [colors[bookmark.cluster] for bookmark in self.manager.bookmarks]
+        plt.scatter(x, y, color=color)
+        # for bookmark, (x, y) in zip(self.manager.bookmarks, self.embeddings_2d):
+        #     color = colors[bookmark.cluster]
+        #     plt.scatter(x, y, color=color)
 
         plt.xlabel("Component 1")
         plt.ylabel("Component 2")
         plt.title("2D Visualization of Sentence Embeddings")
+
+        cursor = mplcursors.cursor(hover=True)
+
+        @cursor.connect("add")
+        def on_add(sel):
+            i = sel.target.index
+            bookmark = self.manager.bookmarks[i]
+            sel.annotation.set_text(bookmark.title)
+            print(bookmark.title, bookmark.url)
+
         plt.show()
 
     def visualize3d(self):
         pca = PCA(n_components=3)
         self.embeddings_3d = pca.fit_transform(self.manager.embeddings[:100])
         print(self.embeddings_3d.shape)
-        colors = plt.cm.rainbow(np.linspace(0, 1, self.n_clusters))
+        colors = plt.cm.rainbow(np.linspace(0, 1, self.manager.n_clusters()))
         fig = plt.figure()
         ax = fig.add_subplot(projection="3d")
 
